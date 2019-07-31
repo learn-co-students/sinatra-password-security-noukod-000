3c7b91fdac7586deef404e4f3bf41252d8321dc6
Alexandro Disla <alexandrodisla@hotmail.com>
2019-07-31 16:26:16 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,17 @@ class ApplicationController < Sinatra::Base
 	end
 
 	post "/signup" do
-		#your code here!
+		#binding.pry
+		#User.create(params)
+		# but we need to implement more behavior
+		user = User.new(:username => params[:username], :password => params[:password])
+		if user.save
+			# because of hash_secure_password
+			# we won't be able to save empty password
+	    redirect "/login"
+	  else
+	    redirect "/failure"
+	  end
 	end
 
 	get "/login" do
@@ -25,8 +35,34 @@ class ApplicationController < Sinatra::Base
 	end
 
 	post "/login" do
-		#your code here!
+		#binding.pry
+		#User.find_by_username(params[:username])
+		user = User.find_by(:username => params[:username])
+=begin
+Let's step through the process of how `User`'s `authenticate` method works. It:
+
+1. Takes a `String` as an argument e.g. `i_luv@byron_poodle_darling`
+2. It turns the `String` into a salted, hashed version (`76776516e058d2bf187213df6917a7e`)
+3. It compares this salted, hashed version with the user's stored salted,
+	 hashed password in the database
+4. If the two versions match, `authenticate` will return the `User` instance;
+	 if not, it returns `false`
+
+> **IMPORTANT** At no point do we look at an unencrypted version of the user's
+> password.
+
+In the code below, we see how we can ensure that we have a `User` AND that that
+`User` is authenticated. If the user authenticates, we'll set the
+`session[:user_id]` and redirect to the `/success` route. Otherwise, we'll
+redirect to the `/failure` route so our user can try again.
+=end
+	if user && user.authenticate(params[:password])
+		session[:user_id] = user.id
+		redirect "/success"
+	else
+	 	redirect "/failure"
 	end
+end
 
 	get "/success" do
 		if logged_in?
